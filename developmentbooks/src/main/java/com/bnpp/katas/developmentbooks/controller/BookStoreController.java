b70bf986b3c06eb27c3f94ce266362180bc0ce13
reshm <reshm@DESKTOP-F10JV1T>
2024-09-24 03:35:51 +0530

    feat : Add REST API to calculate the price of the book

--- developmentbooks/src/main/java/com/bnpp/katas/developmentbooks/controller/BookStoreController.java
+++ developmentbooks/src/main/java/com/bnpp/katas/developmentbooks/controller/BookStoreController.java
@@ -2,24 +2,36 @@ package com.bnpp.katas.developmentbooks.controller;
 
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bnpp.katas.developmentbooks.model.Book;
+import com.bnpp.katas.developmentbooks.model.BookRequest;
 import com.bnpp.katas.developmentbooks.service.BookStoreService;
+import com.bnpp.katas.developmentbooks.service.CalculateBookPriceService;
+
 
 @RestController
 @RequestMapping("/api/bookstore")
 public class BookStoreController {
 
 	private BookStoreService bookStoreService;
+	private CalculateBookPriceService calculateBookPriceService;
 	
-	public BookStoreController(BookStoreService bookStoreService) {
+	public BookStoreController(BookStoreService bookStoreService,CalculateBookPriceService calculateBookPriceService) {
 		this.bookStoreService = bookStoreService;
+		this.calculateBookPriceService = calculateBookPriceService;
 	}
 	
 	@GetMapping("/fetchbooks")
 	public List<Book> fetchAvailableBooks() {
 		return bookStoreService.fetchBooks();
+	}
+	
+	@PostMapping("/calculateprice")
+	public double calculatePrice(@RequestBody List<BookRequest> request) {
+		return calculateBookPriceService.calculatePrice(request);
 	}
 }
--- developmentbooks/src/test/java/com/bnpp/katas/developmentbooks/controller/BookStoreControllerTest.java
+++ developmentbooks/src/test/java/com/bnpp/katas/developmentbooks/controller/BookStoreControllerTest.java
@@ -1,10 +1,10 @@
 package com.bnpp.katas.developmentbooks.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyList;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,15 +16,20 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import com.bnpp.katas.developmentbooks.model.BookRequest;
 import com.bnpp.katas.developmentbooks.service.CalculateBookPriceService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class BookStoreControllerTest {
 
+	private static final int ONE = 1;
+	private static final int TWO = 2;
+
 	@Mock
 	private CalculateBookPriceService calculateBookPriceService;
-	
+
 	@Autowired
 	MockMvc mockMvc;
 
@@ -33,5 +38,18 @@ class BookStoreControllerTest {
 	void fetchAvailableBooks_ShouldRetreiveTheBooks() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/api/bookstore/fetchbooks").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk());
+	}
+
+	@Test
+	@DisplayName("API to calculate the price of book")
+	void fetchPriceSummaary_Api_shouldReturn_StatusOK() throws Exception {
+		List<BookRequest> listOfBooks = new ArrayList<BookRequest>();
+		BookRequest firstBook = new BookRequest(ONE, ONE);
+		BookRequest secondBook = new BookRequest(TWO, TWO);
+		listOfBooks.add(firstBook);
+		listOfBooks.add(secondBook);
+
+		mockMvc.perform(post("/api/bookstore/calculateprice").contentType(MediaType.APPLICATION_JSON)
+				.content(new ObjectMapper().writeValueAsString(listOfBooks))).andExpect(status().isOk());
 	}
 }
