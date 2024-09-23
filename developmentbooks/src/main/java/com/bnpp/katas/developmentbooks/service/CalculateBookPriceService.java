10196949fe6067357a3e5f89ba7c3d85fbcadaf1
reshm <reshm@DESKTOP-F10JV1T>
2024-09-24 01:44:13 +0530

    refactor : Modified the Book Request to accept list of values

--- developmentbooks/src/main/java/com/bnpp/katas/developmentbooks/service/CalculateBookPriceService.java
+++ developmentbooks/src/main/java/com/bnpp/katas/developmentbooks/service/CalculateBookPriceService.java
@@ -1,6 +1,8 @@
 package com.bnpp.katas.developmentbooks.service;
 
 import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.bnpp.katas.developmentbooks.model.BookRequest;
@@ -9,13 +11,15 @@ import com.bnpp.katas.developmentbooks.store.BooksEnum;
 @Service
 public class CalculateBookPriceService {
 
-	public double calculatePrice(BookRequest bookReq) {
-	
-		BooksEnum book = Arrays.stream(BooksEnum.values())
-		            .filter(b -> b.getId() == bookReq.getId())
-		            .findFirst()
-		            .orElse(null);
-		return book.getPrice() * bookReq.getQuantity();
+	public double calculatePrice(List<BookRequest> bookRequest) {
+
+		return bookRequest.stream().mapToDouble(bookReq -> {
+			BooksEnum book = Arrays.stream(BooksEnum.values())
+					.filter(b -> b.getId() == bookReq.getId()).findFirst()
+					.orElse(null);
+			return (book != null) ? book.getPrice() * bookReq.getQuantity() : 0.0;
+		}).sum();
+
 	}
 
 }
--- developmentbooks/src/test/java/com/bnpp/katas/developmentbooks/service/CalculateBookPriceServiceTest.java
+++ developmentbooks/src/test/java/com/bnpp/katas/developmentbooks/service/CalculateBookPriceServiceTest.java
@@ -2,6 +2,9 @@ package com.bnpp.katas.developmentbooks.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import com.bnpp.katas.developmentbooks.model.BookRequest;
@@ -12,9 +15,11 @@ class CalculateBookPriceServiceTest {
 	public void calculatePriceForASingleBookPurchase_ShouldReturnPriceFifty() {
 
 		CalculateBookPriceService calculateBookPriceService = new CalculateBookPriceService();
+		List<BookRequest> listOfBooks = new ArrayList<BookRequest>();
 		BookRequest bookReq = new BookRequest(1, 1);
+		listOfBooks.add(bookReq);
 
-		double price = calculateBookPriceService.calculatePrice(bookReq);
+		double price = calculateBookPriceService.calculatePrice(listOfBooks);
 		
 		assertEquals(50.0, price);
 	}
