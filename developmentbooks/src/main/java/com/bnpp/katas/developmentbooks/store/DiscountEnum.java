c275ca28b16ed30698535f7d9abe4d55e2f53860
reshm <reshm@DESKTOP-F10JV1T>
2024-09-24 03:59:33 +0530

    feat : Add Discount Enum

---
+++ developmentbooks/src/main/java/com/bnpp/katas/developmentbooks/store/DiscountEnum.java
@@ -0,0 +1,20 @@
+package com.bnpp.katas.developmentbooks.store;
+
+import java.util.Arrays;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum DiscountEnum {
+
+	TWO_BOOKS(2, 0.05), 
+	THREE_BOOKS(3, 0.10), 
+	FOUR_BOOKS(4, 0.20), 
+	FIVE_BOOKS(5, 0.25);
+	
+	private final int numberOfDistinctItems;
+    private final double discountPercentage;
+    
+    
+}
