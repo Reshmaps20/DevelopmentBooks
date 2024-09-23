76250faa34c4e8250cae7d935f72a0bb07291bc4
reshm <reshm@DESKTOP-F10JV1T>
2024-09-23 23:41:58 +0530

    refactor: Add lombok in BooksEnum

--- developmentbooks/src/main/java/com/bnpp/katas/developmentbooks/store/BooksEnum.java
+++ developmentbooks/src/main/java/com/bnpp/katas/developmentbooks/store/BooksEnum.java
@@ -1,5 +1,10 @@
 package com.bnpp.katas.developmentbooks.store;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum BooksEnum {
 
 	CLEAN_CODE(1, "Clean Code", "Robert Martin", 2008, 50.00),
@@ -13,52 +18,4 @@ public enum BooksEnum {
 	private String author;
 	private int year;
 	private double price;
-
-	private BooksEnum(int id, String title, String author, int year, double price) {
-		this.id = id;
-		this.title = title;
-		this.author = author;
-		this.year = year;
-		this.price = price;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getAuthor() {
-		return author;
-	}
-
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-
-	public int getYear() {
-		return year;
-	}
-
-	public void setYear(int year) {
-		this.year = year;
-	}
-
-	public double getPrice() {
-		return price;
-	}
-
-	public void setPrice(double price) {
-		this.price = price;
-	}
 }
